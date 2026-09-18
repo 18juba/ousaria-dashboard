ef963c2ad0c4bdca88141e67b770c1df82795673
18juba <jubalucas05@gmail.com>
2026-09-18 20:00:43 -0300

    Modelando estrutura inicial da interface

--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -10,4 +10,23 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<main class="flex min-h-screen h-screen gap-8 p-10">
+	<nav class="flex w-60 flex-col justify-between py-8">
+		<ul class="flex flex-col gap-4 text-lg font-bold text-white">
+			<li class="rounded-lg bg-linear-to-r from-white/20 to-white/5 p-2">
+				<a href="/dashboard">Dashboard</a>
+			</li>
+			<li class="rounded-lg p-2">
+				<a href="/analise">Análise</a>
+			</li>
+			<li class="rounded-lg p-2">
+				<a href="/automacao">Automação</a>
+			</li>
+		</ul>
+
+		<img src="/img/logo.webp" , alt="Logomarca Ousaria" class="w-50 object-contain" />
+	</nav>
+
+	{@render children()}
+</main>
