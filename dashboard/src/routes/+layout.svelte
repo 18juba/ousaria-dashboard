09b4a22547423914208b36b65ec11c35b4308649
18juba <jubalucas05@gmail.com>
2026-09-18 21:16:21 -0300

    Estruturando layout para páginas

--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -15,11 +15,11 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<main class="flex h-screen min-h-screen gap-8 p-6 2xl:p-10">
-	<nav class="flex w-50 2xl:w-60 flex-col justify-between py-8">
-		<ul class="flex flex-col gap-4 2xl:text-lg font-bold text-white">
+<main class="flex h-screen min-h-screen gap-8 p-6">
+	<nav class="flex w-50 flex-col justify-between py-8 2xl:w-60">
+		<ul class="flex flex-col gap-4 font-bold text-white 2xl:text-lg">
 			<li class="rounded-lg bg-linear-to-r from-white/20 to-white/5 p-2">
-				<a class="flex items-center gap-4" href="/dashboard"><ChartNoAxesCombined /> Dashboard</a>
+				<a class="flex items-center gap-4" href=""><ChartNoAxesCombined /> Dashboard</a>
 			</li>
 			<li class="rounded-lg p-2">
 				<a class="flex items-center gap-4" href="/relatorio"><FileText /> Relatório</a>
@@ -29,8 +29,19 @@
 			</li>
 		</ul>
 
-		<img src="/img/logo.webp" , alt="Logomarca Ousaria" class="w-44 2xl:w-50 object-contain" />
+		<img
+			src="/img/logo-slogan.webp"
+			,
+			alt="Logomarca Ousaria"
+			class="w-44 object-contain 2xl:w-50"
+		/>
 	</nav>
 
-	{@render children()}
+	<div class="h-full w-full rounded-2xl bg-mist-100 p-6">
+		<div class="mb-4 flex items-center justify-between rounded-lg bg-primary px-6 py-2 font-bold">
+			<p>Comunidade Voo</p>
+			<p class="text-lg tracking-wide">ousar<span class="text-secondary">!</span>a</p>
+		</div>
+		{@render children()}
+	</div>
 </main>
--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -1,3 +1,8 @@
-<div class="bg-mist-100 h-full w-full rounded-2xl">
-
+<div>
+	<div class="grid grid-cols-4 gap-6">
+		<div class="h-24 w-full rounded-lg bg-tertiary p-4">card 1</div>
+		<div class="h-24 w-full rounded-lg bg-tertiary p-4">card 1</div>
+		<div class="h-24 w-full rounded-lg bg-tertiary p-4">card 1</div>
+		<div class="h-24 w-full rounded-lg bg-tertiary p-4">card 1</div>
+	</div>
 </div>
